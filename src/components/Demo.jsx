ce626ee10978ae44a77ef578a17e0efb1f005e79
M. Fahrur Rizal <fahrurrzl5798@gmail.com>
2024-07-04 18:16:24 +0700

    feat: add list url

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,16 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BsLink } from "react-icons/bs";
 import { useLazyGetSummaryQuery } from "../services/article";
 import { loader } from "../assets";
 import BoxError from "./BoxError";
+import { IoCopyOutline } from "react-icons/io5";
+import { SiTicktick } from "react-icons/si";
+import { IoIosClose } from "react-icons/io";
 
 const Demo = () => {
   const [article, setArticle] = useState({
     url: "",
     summary: "",
   });
+  const [allArticle, setAllArticle] = useState([]);
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const allArticleFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+
+    if (allArticleFromLocalStorage) {
+      setAllArticle(allArticleFromLocalStorage);
+    }
+  }, []);
+
+  const handleCopy = (articleUrl) => {
+    navigator.clipboard.writeText(articleUrl);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,12 +38,15 @@ const Demo = () => {
       const newArticle = { ...article, summary: data.summary };
 
       setArticle(newArticle);
-
-      console.log(newArticle);
     }
   };
 
-  console.log({ error });
+  const handleClose = () => {
+    setArticle({
+      url: "",
+      summary: "",
+    });
+  };
 
   return (
     <section className="w-full max-w-xl mt-12">
@@ -40,7 +61,7 @@ const Demo = () => {
           />
           <input
             type="url"
-            placeholder="Masukkan URL nya cuy..."
+            placeholder="Input a URL"
             onChange={(e) => setArticle({ ...article, url: e.target.value })}
             value={article.url}
             required
@@ -55,6 +76,24 @@ const Demo = () => {
           </button>
         </form>
         {/* Browse url */}
+        <div className="max-h-56 p-1 overflow-y-auto wrap-url">
+          {allArticle?.map((item, index) => (
+            <div
+              key={`item-${index}`}
+              className="p-2 rounded-sm text-sm bg-white/40 backdrop-blur-sm text-slate-500 truncate mb-2"
+              onClick={() => setArticle(item)}
+            >
+              <button
+                type="button"
+                className="text-slate-500 flex items-center justify-center gap-2"
+                onClick={() => handleCopy(item.url)}
+              >
+                <IoCopyOutline size={18} />
+                {item.url}
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
 
       {/* Result */}
@@ -66,10 +105,17 @@ const Demo = () => {
         <BoxError message={error?.data.error} />
       ) : article?.summary ? (
         <div className="box-summary">
-          <div className="mb-3">
+          <div className="mb-3 flex items-center justify-between">
             <h1 className="text-lg text-slate-600 font-bold">
               Article <span className="blue-gradient">Summary:</span>
             </h1>
+            <button
+              type="button"
+              className="p-1 text-red-500 hover:text-red-600 transition-all duration-300"
+              onClick={handleClose}
+            >
+              <IoIosClose size={24} />
+            </button>
           </div>
           <article>
             <p className="text-slate-500 leading-relaxed text-pretty break-words text-base sm:text-lg">
